cd3ca29a9e27298e27caff43183bad6a786286b9
Vishwas <Vishwas@gmail.com>
2013-08-01 17:30:48 +0000

    Modified message to be read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var app = express();
+
+
+var fs = requre('fs');
+var indexfile = 'index.html';
+var data = fs.readFileSync(indexfile);
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
