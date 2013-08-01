87b716c346fbc32631d2790c71cd462683191f4a
Vishwas <Vishwas@gmail.com>
2013-08-01 23:57:16 +0000

    Updated code with port 8080

--- web.js
+++ web.js
@@ -1,20 +1,15 @@
 var express = require('express');
-var app = express();
-
-
 var fs = require('fs');
-var indexfile = 'index.html';
-var data = fs.readFileSync(indexfile);
+var htmlfile = 'index.html';
 
-app.use(express.logger());
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write(data);
-  response.end();
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
